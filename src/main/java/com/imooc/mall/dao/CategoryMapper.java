dfe771678b9e3bf7d82942491d6f6d351844fc66
chid89123 <zishuo@nwafu.edu.cn>
2023-04-15 19:37:56 +0800

    初始化项目

---
+++ src/main/java/com/imooc/mall/dao/CategoryMapper.java
@@ -0,0 +1,17 @@
+package com.imooc.mall.dao;
+
+import com.imooc.mall.pojo.Category;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Param;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+//@Mapper
+public interface CategoryMapper {
+
+    @Select("select * from mall_category where id = #{id}")
+    Category findById(@Param("id") Integer id);
+
+    Category queryById(Integer id);
+}
